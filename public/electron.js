ca724ac699b90ef523effec223a0cc4749e24061
tonnac <tonnac35@gmail.com>
2021-07-10 03:05:33 +0900

    Electron

---
+++ public/electron.js
@@ -0,0 +1,59 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron')
+const isDev = require('electron-is-dev')
+const path = require('path')
+
+let mainWindow;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    // 이것들은 제가 사용하는 설정이니 각자 알아서 설정 하십시오.
+    //alwaysOnTop: true,
+    center: true,
+    //fullscreen: true,
+    kiosk: !isDev,
+    resizable: true,
+    webPreferences: {
+      // 2.
+      // 웹 애플리케이션을 데스크탑으로 모양만 바꾸려면 안 해도 되지만,
+      // Node 환경처럼 사용하려면 (Node에서 제공되는 빌트인 패키지 사용 포함)
+      // true 해야 합니다.
+      nodeIntegration: true
+    }
+  });
+
+  if (isDev) {
+    // 개발 중에는 개발 도구에서 호스팅하는 주소에서 로드
+    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.webContents.openDevTools();
+  } else {
+    // 프로덕션 환경에서는 패키지 내부 리소스에 접근
+    mainWindow.loadFile(path.join(__dirname, '../build/index.html'));
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
